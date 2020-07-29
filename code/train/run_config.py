5a4cf8629c4f077957ebc913289fa4866b8fae84
Ayaz43 <65758480+Ayaz43@users.noreply.github.com>
2020-07-29 14:30:51 +0530

    Update run_config.py

--- code/train/run_config.py
+++ code/train/run_config.py
@@ -7,7 +7,7 @@ def main(workspace):
     print("Loading compute target")
     compute_target = ComputeTarget(
         workspace=workspace,
-        name="mycluster"
+        name="aml-compute"
     )
 
     # Load script parameters
